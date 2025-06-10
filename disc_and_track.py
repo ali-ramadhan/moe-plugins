987e39abf46d77a6ff5147a7076e6a4ff46f0dc1
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2025-06-10 06:41:54 -0600

    Use disc number in multi-disc track paths

--- disc_and_track.py
+++ disc_and_track.py
@@ -2,7 +2,7 @@ import moe
 
 @moe.hookimpl
 def create_path_template_func():
-    return [disc_dir_padded]
+    return [disc_dir_padded, disc_and_track_num]
 
 def disc_dir_padded(track, album):
     if album.disc_total == 1:
@@ -11,5 +11,15 @@ def disc_dir_padded(track, album):
         return f"Disc {track.disc:1d}"
     elif 10 <= album.disc_total <= 99:
         return f"Disc {track.disc:02d}"
-    elif 101 <= album.disc_total <= 999:
+    else:
         return f"Disc {track.disc:03d}"
+
+def disc_and_track_num(track, album):
+    if album.disc_total == 1:
+        return f"{track.track_num:02d}"
+    elif 2 <= album.disc_total <= 9:
+        return f"{track.disc:1d}-{track.track_num:02d}"
+    elif 10 <= album.disc_total <= 99:
+        return f"{track.disc:02d}-{track.track_num:02d}"
+    else:
+        return f"{track.disc:03d}-{track.track_num:02d}"
